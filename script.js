ca68edaa76a34ebe55db70abcf8dd53d62396c67
Ritesh Rohilla <riteshrohilla4@gmail.com>
2023-06-07 23:52:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,10 @@ document.querySelector('.guess').value = 23;
 */
 
 // Handling click events
-const number = Math.trunc(Math.random() * 20) + 1;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
 
@@ -20,5 +23,11 @@ document.querySelector('.check').addEventListener('click', function () {
 
     if (!guess) {
         document.querySelector('.message').textContent = '😿 No number selected'
+    } else if (guess === secretNumber) {
+        document.querySelector(".message").textContent = 'Correct Number';
+    } else if (guess > secretNumber) {
+        document.querySelector('.message').textContent = 'Too High!';
+    } else if (guess < secretNumber) {
+        document.querySelector('.message').textContent = 'Too Low!';
     }
 });
