dc74e626dfd9e3374ec0ae2ca1ad2d0f42e27c20
Ritesh Rohilla <riteshrohilla4@gmail.com>
2023-06-09 13:49:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -65,6 +65,13 @@ document.querySelector('.check').addEventListener('click', function () {
 
 */
 
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
 
 
@@ -74,6 +81,25 @@ document.querySelector('.check').addEventListener('click', function () {
 
     if (!guess) {
         document.querySelector('.message').textContent = 'No number!';
-    }
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = 'Correct Number!!!'
+    } else if (guess > secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = 'Too High!'
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You lost the game!'
+            // document.querySelector('.score').textContent = 0;
+        }
+    } else if (guess < secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = 'Too Low!'
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You lost the game!';
+        }
 
+    }
 });
