6077f0975ffeca0d3e6d51be09f2fb146fe48cda
Ritesh Rohilla <riteshrohilla4@gmail.com>
2023-06-11 10:35:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,13 +16,19 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
+const displayMessage = function (message) {
+    document.querySelector('.message').textContent = message;
+
+}
+
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
 
     console.log(guess, typeof guess);
     // When there is no input
     if (!guess) {
-        document.querySelector('.message').textContent = 'No number!';
+        // document.querySelector('.message').textContent = 'No number!';
+        displayMessage('No number!')
 
         // when player wins
     } else if (guess === secretNumber) {
