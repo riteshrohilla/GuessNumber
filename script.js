4fa30bbd5541e264a50101a602192e474f32466b
Ritesh Rohilla <riteshrohilla4@gmail.com>
2023-08-18 23:30:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,6 @@
 console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent = 'Correct number!'
+
+document.querySelector('.number').textContent = 7;
+document.querySelector('.score').textContent = 42;
