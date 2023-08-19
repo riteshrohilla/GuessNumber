22841d2354af9b7aee0ed102e78e576c33e997e7
Ritesh Rohilla <riteshrohilla4@gmail.com>
2023-08-19 22:05:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,5 +7,5 @@ document.querySelector('.message').textContent = 'Correct number!'
 document.querySelector('.number').textContent = 7;
 document.querySelector('.score').textContent = 42;
 
-console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
