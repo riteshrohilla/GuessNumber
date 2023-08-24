ab1d79c7c5e00e9086280417fb332353d04254ac
Ritesh Rohilla <riteshrohilla4@gmail.com>
2023-08-24 15:06:39 +0530

    modifying file

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ console.log(document.querySelector('.guess').value); */
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = document.querySelector('.guess').value;
-    console.log(guess);
+    console.log(typeof guess);
 
     document.querySelector('.message').textContent = 'Correct number!'
 
