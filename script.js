7a4d069bc4c42331f00dd4859db354089a4c095f
Ritesh Rohilla <riteshrohilla4@gmail.com>
2023-09-07 00:57:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,4 +14,9 @@ console.log(document.querySelector('.guess').value);
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
+
+    if (!guess) {
+        document.querySelector('.message').textContent = 'No number!'
+    }
+
 })
