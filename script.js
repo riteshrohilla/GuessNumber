efc8da38516f3396098c7a1fa298e37355ba67e9
Ritesh Rohilla <riteshrohilla4@gmail.com>
2023-09-08 14:26:11 +0530

    -m

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
+const number = Math.trunc(Math.random() * 20) + 1;
+
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
