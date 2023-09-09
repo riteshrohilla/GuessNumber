1abff0e899508204792861694a376a93683ed3ef
Ritesh Rohilla <riteshrohilla4@gmail.com>
2023-09-09 13:13:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -43,6 +43,9 @@ document.querySelector('.check').addEventListener('click', function () {
         }
     }
     document.querySelector('.again').addEventListener('click', function () {
+
+        score = 20;
+        secretNumber = Math.trunc(Math.random() * 20) + 1;
         document.querySelector('.message').textContent = 'Start guessing...';
         document.querySelector('.score').textContent = '20';
         document.querySelector('.number').textContent = '?';
